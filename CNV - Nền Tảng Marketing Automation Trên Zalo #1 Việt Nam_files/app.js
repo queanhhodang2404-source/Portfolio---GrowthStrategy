82cee8dd64f6d98d62e10a0491a84cce001a64aa
queanhhodang2404-source <queanhhodang2404@gmail.com>
2026-04-17 21:18:01 +0700

    HEHE
    HEHE

---
+++ CNV - Nền Tảng Marketing Automation Trên Zalo #1 Việt Nam_files/app.js
@@ -0,0 +1,19 @@
+
+if (!window._subiz_init_2094850928430) {
+window._subiz_init_2094850928430 = true
+window._subiz_init_2094850928430_keep_loading = true
+var tag = document.createElement('script')
+tag.async = 1
+tag.onload = function() { if (typeof sbz_info !== 'undefined') sbz_info["boot"] = Date.now() }
+tag.src = "https://vcdn.subiz-cdn.com/widget-v4/public/19d8a4181b9.app.js"
+
+if (typeof sbz_info !== 'undefined') sbz_info["ready_state"] = document.readyState
+
+if (document.readyState === "interactive" || document.readyState === "complete") {
+  if (typeof sbz_info !== 'undefined') sbz_info["added"] = Date.now()
+  document.body.appendChild(tag)
+} else window.addEventListener("DOMContentLoaded", function(event) {
+  if (typeof sbz_info !== 'undefined') sbz_info["added"] = Date.now()
+	document.body.appendChild(tag)
+})
+} // acrzvklawexwmhykafyb 19d8a4181b9 19d8a41ce33-19d8a4181b9
